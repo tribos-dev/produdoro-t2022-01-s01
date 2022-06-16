eaf53f171ce1cab2021bfab896c7a1ecdb7f70b5
Danilo1dev <danilofoxlog@gmail.com>
2022-06-16 18:43:04 -0300

    Implementado metodo Get em tarefaAPI

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,5 +1,19 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-public interface TarefaAPI {
+import java.util.List;
+import java.util.UUID;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public interface TarefaAPI {
+		
+		@GetMapping ("/{idTarefa}/tarefa")
+		@ResponseStatus(code = HttpStatus.OK)
+		List<TarefaDTO> listaTarefaPorUsuario(@Valid @PathVariable UUID idTarefa); 
+	
 }
