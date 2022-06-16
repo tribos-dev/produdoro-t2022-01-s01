a3896315927c01cf8eb4c586ffa88c56e7f49a2c
marcos xavier <sis.marcosxavier@gmail.com>
2022-06-16 17:48:25 -0300

    implementa metodo adicionaTarefa em TarefaAPI e TarefaRestController

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,5 +1,19 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-public interface TarefaAPI {
+import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1/tarefa")
+@RestController
+public interface TarefaAPI {
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	TarefaDTO adicionaTarefa (@RequestBody @Valid TarefaFORM tarefaFORM);
 }
--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -1,5 +1,22 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.wakandaacademy.produdoro.tarefa.application.service.TarefaService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
 public class TarefaRestController implements TarefaAPI {
+	
+	private TarefaService tarefaService;
+
+	@Override
+	public TarefaDTO adicionaTarefa(@Valid TarefaFORM tarefaFORM) {
+		TarefaDTO novaTarefa = tarefaService.adicionaTarefa(tarefaFORM.toEntity());
+		return novaTarefa;
+	}
 
 }
