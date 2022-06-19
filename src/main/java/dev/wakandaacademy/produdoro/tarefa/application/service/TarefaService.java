ba62953aa7576d663fd3402bd1bf83309ee9107d
Danilo1dev <danilofoxlog@gmail.com>
2022-06-19 17:37:36 -0300

    Implementar método buscaTarefas em TarefaService e TarefaSpringMongoDBService

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
@@ -1,5 +1,14 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
 public class TarefaService implements TarefaApplicationService {
+
+	List<Tarefa> buscaTarefaPorIdUsuario(@Valid UUID idUsuario);
 
 }
--- src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaRepositoryMongoDB.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaRepositoryMongoDB.java
@@ -1,7 +1,16 @@
 package dev.wakandaacademy.produdoro.tarefa.infra;
 
-import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import java.util.List;
+import java.util.UUID;
 
-public class TarefaRepositoryMongoDB  implements TarefaRepository{
+import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
+public class TarefaRepositoryMongoDB implements TarefaRepository {
+
+	public List<Tarefa> buscaTarefaPorIdUsuario(UUID IdUsuario) {
+		List<Tarefa> listaTarefas = tarefaMongoDBSpringRepository.findAllByIdUsuario(IdUsuario);
+		return listaTarefas;
+	}
 
 }
