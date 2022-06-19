11cdcb9c2da3f57cf757cc3ab56d4f2768e6fda0
Danilo1dev <danilofoxlog@gmail.com>
2022-06-19 17:21:51 -0300

    Implementado metodo buscaTarefa em TarefaRepository

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
@@ -1,5 +1,14 @@
 package dev.wakandaacademy.produdoro.tarefa.application.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
 public interface TarefaRepository {
+
+	List<Tarefa> buscaTarefaPorIdUsuario(@Valid UUID IdUsuario);
 
 }
