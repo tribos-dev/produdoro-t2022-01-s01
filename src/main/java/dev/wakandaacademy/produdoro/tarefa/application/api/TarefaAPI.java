f565a5385ed6c753932ff542ca48f184dbd880d8
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-06-19 21:44:19 -0300

    implementado metodo PUT no TarefaAPI e no TarefaRestController

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,5 +1,19 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 public interface TarefaAPI {
 
+	@PutMapping("/{idTarefa}")
+	@ResponseStatus (code=HttpStatus.NO_CONTENT)
+	public void editaTarefa (@PathVariable UUID idTarefa, @RequestBody @Valid TarefaFORM tarefaForm);
 }
--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -1,5 +1,21 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dev.wakandaacademy.produdoro.tarefa.application.service.TarefaService;
+
 public class TarefaRestController implements TarefaAPI {
+	
+	@Autowired
+	private TarefaService tarefaService;
+
+	@Override
+	public void editaTarefa(UUID idTarefa, @Valid TarefaFORM tarefaForm) {
+		tarefaService.editaTarefa(idTarefa, tarefaForm.toEntity());
+	}
 
 }
