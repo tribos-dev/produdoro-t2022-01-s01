627ca454d09da080460ae75da76a0573a39f012a
marcos xavier <sis.marcosxavier@gmail.com>
2022-06-22 14:03:24 -0300

    correção de import na classe TarefaRestController

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.wakandaacademy.produdoro.tarefa.application.service.TarefaApplicationService;
-import dev.wakandaacademy.produdoro.tarefa.application.service.TarefaService;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
