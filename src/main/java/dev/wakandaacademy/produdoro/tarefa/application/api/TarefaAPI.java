1721f1c6650f51b182d2fb2118e19b76169a2121
marcos xavier <sis.marcosxavier@gmail.com>
2022-06-23 08:30:26 -0300

    correção do mapeamento em TarefaAPI

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/v1/tarefa")
+@RequestMapping("/v1/tarefas")
 @RestController
 public interface TarefaAPI {
 	
